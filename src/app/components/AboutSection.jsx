ea13890b9f84c04fb5fae8e52129e97d88566dfd
abdelillahbel <73066920+abdelillahbel@users.noreply.github.com>
2024-09-09 11:57:28 +0100

    first commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,99 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Android Development (Jetpack Compose, Java, Kotlin)</li>
+        <li>Web Development (Next.js, React, TypeScript)</li>
+        <li>Firebase (Firestore, Authentication, Storage)</li>
+        <li>Cloud Deployment (Google Cloud Platform - GCP)</li>
+        <li>Database Management (MySQL, MongoDB, SQL Server)</li>
+        <li>Version Control (Git, GitHub)</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>IT Degree - BTS, Algeria</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Google Developer Student Clubs - Core Team Member & Software Developer Lead</li>
+        <li>Java Developer Certification</li>
+      </ul>
+    ),
+  },
+  
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image alt="setup" src="/images/setup.jpg" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I&apos;m a software developer with a strong focus on Android, web, and cloud development.
+            I have experience working with technologies like Firebase, GCP, and Jetpack Compose.
+            I thrive on solving complex challenges and delivering high-quality solutions.
+            Over the years, I&apos;ve built various apps for startups and enterprises.
+            Beyond coding, I enjoy mentoring others and sharing knowledge in tech communities.
+            My goal is to continuously grow while creating impactful software solutions.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
